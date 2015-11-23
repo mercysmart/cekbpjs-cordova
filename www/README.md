bee41068b15f91df4f9f31a15db53a589c4496fe
christoga <christogaandre@gmail.com>
2015-11-23 19:34:18 +0700

    Initial commit

---
+++ www/README.md
@@ -0,0 +1,14 @@
+<div align="center"><img src="img/logo.png"></div>
+# CekBPJS
+Merupakan sebuah layanan BPJS yang akan membantu anda dalam membooking kamar rumah sakit
+
+## Tim CekBPJS
+* Mercy Sihombing - CMO
+* Christie Kirana - CEO
+* Andre Christoga - CTO
+
+## Demo
+You can check out the demo at http://git.io/cekbpjs or [http://mercysmart.github.io/cekbpjs-app](http://mercysmart.github.io/cekbpjs-app)
+
+## License
+CekBPJS di lisensi oleh MIT.
---
+++ www/js/main.js
@@ -0,0 +1,36 @@
+function rate() {
+	window.alert("Terima kasih untuk penilaian yang anda berikan, Kami akan meninjau banding dan merespon dalam waktu beberapa hari.");
+	window.location.href='index.html'
+}
+
+// if (typeof Hospital == 'undefined') {
+// 	throw new Error ('Mohon maaf, Rumah Sakit di daerah anda tidak terdeteksi')
+// } else {
+
+// };
+
+function call(){
+	window.location.href='rate.html';
+}
+
+function pesan(){
+	window.alert("Mohon masukan data yang benar.")
+	window.location.href='kamar.html'
+}
+// if (muhammadiyah) {
+// 	var name = Muhammadiyah
+// 	var img = img/rs/muhammadiyah.JPG
+// 	var tel = +625363244801;
+// };
+
+// if (bhayangkara) {
+// 	var name = Bhayangkara
+// 	var img = img/rs/bhayangkara.jpg
+// 	var tel = +625363221520;
+// };
+
+// if (doris) {
+// 	var name = Doris
+// 	var img = img/rs/doris.jpg
+// 	var tel = +625363308671
+// };
